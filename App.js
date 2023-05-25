b949f51f7f3e63045f9dfe1dac7e9848aa490225
seulgi <guseul9405@gmail.com>
2023-05-25 16:47:03 +0900

    날씨앱 연습 -  디바이스 크기를 불러와서 paging 처리

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Dimensions } from 'react-native';
+
+const { height: deviceHeight, width:deviceWidth } = Dimensions.get('window');
 
 export default function App() {
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Seoul</Text>
+        </View>
+        <ScrollView 
+          pagingEnabled
+          horizontal 
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.weather}>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </ScrollView>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex : 1,
+    backgroundColor: '#abdbe3',
+  },
+  city : {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName : {
+    color: 'black',
+    fontSize: 36,
+    fontWeight: '600',
+  },
+  weather : {
+  },
+  day : {
+    width : deviceWidth,
+    alignItems: 'center',
+  },
+  temp : {
+    fontSize: 120,
+    fontWeight: 600,
+    marginTop : 50,
+  },
+  description: {
+    fontSize: 30,
+    marginTop: -10,
   },
 });

303962012faa16e455e142425a6de8619e734a52
seulgi <guseul9405@gmail.com>
2023-05-26 12:42:45 +0900

    Location 을 활용한 위치 정보 불러오기

--- App.js
+++ App.js
@@ -1,16 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+
 
 const { height: deviceHeight, width:deviceWidth } = Dimensions.get('window');
 
 export default function App() {
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const [city, setCity] = useState("Loading...");
+
+  const getWeahter = async() => {
+    try {
+      const {granted} = await Location.requestForegroundPermissionsAsync();
+
+      // 권한을 받지 못했을 때
+      if(!granted) {
+        setOk(false);
+      }
   
+      // 권한을 성공적으로 받으면
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 3 });
+  
+      const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
+      setCity(location[0].city);
+    } catch (err ) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getWeahter();
+  }, []);
 
   return (
       <View style={styles.container}>
         <View style={styles.city}>
-          <Text style={styles.cityName}>Seoul</Text>
+          <Text style={styles.cityName}>{city}</Text>
         </View>
         <ScrollView 
           pagingEnabled

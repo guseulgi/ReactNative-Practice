f752c952f777fe33bade51ebb9154866b1bb042f
seulgi <guseul9405@gmail.com>
2023-05-30 15:45:49 +0900

    삭제 기능 추가

--- pages/TravelAndTodo.js
+++ pages/TravelAndTodo.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, Text,
-  TouchableOpacity, TextInput,
+  TouchableOpacity, TextInput, ScrollView, Pressable,
+  Alert, ActivityIndicator,
 } from 'react-native';
 import { theme } from '../utils/colors';
 import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AntDesign } from '@expo/vector-icons';
+
+const STORAGE_KEY = '@todos';
 
 export default function TravelAndTodo() {
   const [working, setWorking] = useState(true);
@@ -13,26 +18,69 @@ export default function TravelAndTodo() {
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
 
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
   const onChangeText = (payload) => {
     setText(payload);
   }
 
-  const addTodo = () => {
+  const saveTodos = async (val) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(val));
+    } catch(err) {
+      throw err;
+    }
+  }
+
+  const removeTodos = async (key) => {
+    try {
+      Alert.alert('✔️ 안내', `정말로 '${todos[key].text}' 내용을 삭제하시겠습니까?`, [
+        {
+          text: '취소',
+          style: 'cancel',
+        },
+        { 
+          text: '삭제', 
+          onPress: async () => {
+            Reflect.deleteProperty(todos, key);
+            await saveTodos(todos);
+            loadTodos();
+          },
+          style: 'destructive'
+        }
+      ]);
+    } catch(err) {
+      throw err;
+    }
+  }
+  
+  const loadTodos = async () => {
+    try {
+      const result = await AsyncStorage.getItem(STORAGE_KEY);
+      const data = JSON.parse(result);
+      data !== null ? setTodos(data) : null;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  const addTodo = async () => {
     if (text === '') return;
 
-    const newTodos = Object.assign({}, todos,
-      {
-        [Date.now()] : {
-          text,
-          work:working,
-        }
-      });
+    const newTodos = { 
+      ...todos,
+      [Date.now()] : {
+        text,
+        working }
+    };
+
     setTodos(newTodos);
+    await saveTodos(newTodos);
     setText('');
   }
 
-  console.log(todos);
-  
   return (
     <View style={styles.container}>
       <StatusBar style='light' />
@@ -55,7 +103,32 @@ export default function TravelAndTodo() {
         onSubmitEditing={addTodo} 
         onChangeText={onChangeText}
       />
-
+      <ScrollView 
+        showsVerticalScrollIndicator={false}
+      >
+        {todos === {} 
+          ? <ActivityIndicator size='large' color='white' style={{ marginTop: 70, }}/> 
+          : Object.keys(todos).map((el) => {
+          return (
+            todos[el].working === working ?
+              <View 
+                key={el}
+                style={styles.todo} 
+              >
+                <Text style={styles.todoText}>
+                  {todos[el].text}
+                </Text>
+                <TouchableOpacity    
+                  onPress={() => removeTodos(el, todos[el])}
+                  activeOpacity={0.6}
+                >
+                  <AntDesign name="delete" size={18} color="white" />
+                </TouchableOpacity>
+              </View>
+            : null
+          );
+        })}
+      </ScrollView>
     </View>
   )
 }
@@ -82,5 +155,21 @@ const styles = StyleSheet.create({
     borderRadius : 30,
     marginTop : 20,
     fontSize : 18,
-  }
+    marginVertical: 40,
+  },
+  todo : {
+    flexDirection:'row',
+    alignItems: 'center',
+    justifyContent:'space-between',
+    backgroundColor : theme.todoBg,
+    marginBottom: 15,
+    paddingVertical : 15,
+    paddingHorizontal: 18,
+    borderRadius : 15,
+  },
+  todoText : {
+    color: 'white',
+    fontSize : 18,
+    fontWeight: 400,
+  },
 });
